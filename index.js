977045237dcf4b407028b2718e26c12aeb8bd446
S.Mohammad Hosseininejad <smohammadhnd@gmail.com>
2023-02-07 18:30:44 +0330

    feat: make a simple get method

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 
+// get methods
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.send('Welcome to my version of Vidly')
 })
+
+// post methods
+// put methods
+// delete methods
 
 const port = process.env.PORT || 8000
 app.listen(port, () => console.log(`Listening on port ${port} ...`))
