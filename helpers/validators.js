6974da319606b3fbf5aa08f337ceb38e71bb96a2
S.Mohammad Hosseininejad <smohammadhnd@gmail.com>
2023-02-16 11:31:21 +0330

    feat(helpers/validators): add customer schema validation

--- helpers/validators.js
+++ helpers/validators.js
@@ -1,7 +1,15 @@
 const Joi = require('joi')
 
 module.exports = {
-  // validate user request body
+  // prevent running the application without setting environment variables being set
+  checkEnvironmentVariables() {
+    if (!process.env.PORT)
+      throw new Error(
+        'PORT env not found, Have you forgotten to set your environment variables?'
+      )
+  },
+
+  // genre user input validation
   checkGenreSchema(genre) {
     const schema = Joi.object({
       name: Joi.string().required().min(3).max(30),
@@ -15,11 +23,19 @@ module.exports = {
     }
   },
 
-  // prevent running the application without setting environment variables being set
-  checkEnvironmentVariables() {
-    if (!process.env.PORT)
-      throw new Error(
-        'PORT env not found, Have you forgotten to set your environment variables?'
-      )
+  // customer user input validation
+  checkCustomerSchema(customer) {
+    const schema = Joi.object({
+      isGold: Joi.boolean(),
+      name: Joi.string().required().min(3).max(30),
+      phone: Joi.string().required().regex(/^09.*/),
+    })
+
+    const { error } = schema.validate(customer)
+
+    return {
+      valid: error == null,
+      message: error ? error.details[0].message : null,
+    }
   },
 }
