5868f848c518ba8ccd4c3c430c3b49cf470d256a
S.Mohammad Hosseininejad <smohammadhnd@gmail.com>
2023-02-18 19:00:52 +0330

    feat(models): add rentals

---
+++ models/rentals.js
@@ -0,0 +1,53 @@
+const Joi = require('joi')
+const mongoose = require('mongoose')
+const config = require('config')
+
+const { customerSchema } = require(config.get('path') + '/models/customers')
+const { movieSchema } = require(config.get('path') + '/models/movies')
+
+const rentalSchema = new mongoose.Schema({
+  customer: {
+    type: customerSchema,
+    required: true,
+  },
+
+  movie: {
+    type: movieSchema,
+    required: true,
+  },
+
+  dateOut: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+
+  dateReturned: {
+    type: Date,
+  },
+
+  rentalFee: {
+    type: Number,
+    min: 0,
+  },
+})
+
+const Rental = mongoose.model('Rental', rentalSchema)
+
+function rentalValidate(rental) {
+  const schema = Joi.object({
+    movieId: Joi.string().required(),
+    customerId: Joi.string().required(),
+  })
+
+  const { error } = schema.validate(rental)
+
+  return {
+    valid: error == null,
+    message: error ? error.details[0].message : null,
+  }
+}
+
+exports.Rental = Rental
+exports.rentalSchema = rentalSchema
+exports.rentalValidate = rentalValidate
