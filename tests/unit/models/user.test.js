1cfcfe073ea5eb1b9e81766e5d2b7e13d4283a56
S.Mohammad Hosseininejad <smohammadhnd@gmail.com>
2023-03-16 17:47:41 +0330

    feat(test): add test for generateAuthToken

---
+++ tests/unit/models/user.test.js
@@ -0,0 +1,19 @@
+const config = require('config')
+const { User } = require('../../../models/users')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+
+describe('user.generateAuthToken', () => {
+  it('should return a valid JWT token', () => {
+    const payload = {
+      _id: new mongoose.Types.ObjectId().toHexString(),
+      isAdmin: true,
+    }
+    const user = new User(payload)
+    const token = user.generateAuthToken()
+
+    const decoded = jwt.verify(token, config.get('jwtSecret'))
+    console.log(config.get('jwtSecret'))
+    expect(decoded).toMatchObject(payload)
+  })
+})
